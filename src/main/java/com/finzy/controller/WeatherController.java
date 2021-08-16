0f53c6230c0a1e879d2657fe0f60fef310d3d043
DhareppaS1 <DhareppaS1@verifone.com>
2021-08-16 22:38:11 +0530

    Removed UnUsed Imports

--- src/main/java/com/finzy/controller/WeatherController.java
+++ src/main/java/com/finzy/controller/WeatherController.java
@@ -1,6 +1,5 @@
 package com.finzy.controller;
 
-import com.finzy.model.forcast.ForecastResponse;
 import com.finzy.model.weather.WeatherResponse;
 import com.finzy.service.WeatherServiceInterface;
 import com.finzy.utils.GenerateUrl;
@@ -10,14 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import static java.util.Objects.nonNull;
 
